84af08ab74156520bff65d2a3ce031732a362f59
Sayid Sidqi <sayid.sidqi@accenture.com>
2023-09-07 23:11:01 +0700

    #2 add log to ping api

--- src/main/java/id/spring/grafana/api/PingApi.java
+++ src/main/java/id/spring/grafana/api/PingApi.java
@@ -1,17 +1,20 @@
 package id.spring.grafana.api;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
+@Slf4j
 @RestController
 @RequestMapping("/ping")
 public class PingApi {
     
     @GetMapping
     public Map<String, String> pong() {
+        log.info("ping request received");
         return Map.of("data", "pong");
     }
     
