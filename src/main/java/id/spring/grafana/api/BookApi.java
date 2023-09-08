31c9af36f1b9740dbc90c5dabc55f3568f1122e4
Sayid Sidqi <sayid.sidqi@accenture.com>
2023-09-08 13:34:27 +0700

    #7 add crud slow API

--- src/main/java/id/spring/grafana/api/BookApi.java
+++ src/main/java/id/spring/grafana/api/BookApi.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequiredArgsConstructor
@@ -30,6 +31,12 @@ public class BookApi {
     
     @GetMapping
     public List<Book> findBooks(@RequestParam String author, Pageable page) {
+        return bookRepo.findByAuthor(author, page).getContent();
+    }
+    
+    @GetMapping("/_slow")
+    public List<Book> findBooksSlowMode(@RequestParam String author, Pageable page) throws InterruptedException {
+        TimeUnit.MILLISECONDS.sleep((long) ((Math.random() + 1) * 1000));
         return bookRepo.findByAuthor(author, page).getContent();
     }
     
